38731779fa8b637f0f08cb67e574942e6ea2aa21
Hai Minh <minhcm24v7x220@dttx.ctu.edu.vn>
2025-11-05 17:27:11 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "@/components/AppHeader.vue";
 </script>
 
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+
 
 <script>
 export default {
+  components: {
+ AppHeader,
+ },
 }
 </script>
 <template>
 <h1>Hello, Vue.js!</h1>
+
+<div id="app">
+<AppHeader />
+ <div class="container mt-3">
+ <router-view />
+ </div>
+ </div>
 </template>
+
+
 <style>
 .page {
 max-width: 400px;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import router from "./router";
+createApp(App).use(router).mount("#app");
+
 
 createApp(App).mount('#app')
