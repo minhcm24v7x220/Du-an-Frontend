f02f3cc2f5701c2fad62e8e7d5aa3e3959e80644
Hai Minh <minhcm24v7x220@dttx.ctu.edu.vn>
2025-11-05 17:37:05 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,15 @@ import { createWebHistory, createRouter } from "vue-router";
  name: "contactbook",
  component: ContactBook,
  },
+
+{
+ path: "/:pathMatch(.*)*",
+ name: "notfound",
+ component: () => import("@/views/NotFound.vue"),
+},
  ];
+
+
  const router = createRouter({
  history: createWebHistory(import.meta.env.BASE_URL),
  routes,
